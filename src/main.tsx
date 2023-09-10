93f1e747d8dea98a1d19913c34c804f395a0eb7e
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-09-10 14:46:16 +0700

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './components/Header';
+import Button from './components/Button';
+import './style.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<Header />
+		<Button />
+	</React.StrictMode>
+);
