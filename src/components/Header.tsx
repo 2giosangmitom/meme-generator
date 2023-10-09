e7d6093ed5d04296bb2df5d4b24d2e9fe2da4e66
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-10-09 18:21:22 +0700

    update repo link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import troll from '/troll.webp';
 export default function Header() {
 	return (
 		<header className="header">
-			<a href="https://github.com/yeuxacucodon/meme_generator" target="_blank">
+			<a href="https://github.com/2giosangmitom/meme_generator" target="_blank">
 				<img className="header--image" src={troll} alt="troll face" />
 			</a>
 			<h2 className="header--title">Meme generator</h2>
