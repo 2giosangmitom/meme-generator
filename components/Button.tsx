be2bbb5b25f88e78f2170134e909eaf8e491ec37
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-10-30 23:55:59 +0700

    update

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,16 +4,15 @@ import Image from "next/image";
 import { memes_image } from "./Memes";
 
 export default function Button() {
-  const [meme, setMeme] = useState<string>("");
-
-  function handleClick() {
-    const random = Math.floor(Math.random() * memes_image.length);
-    setMeme(memes_image[random]);
-  }
+  const random = Math.floor(Math.random() * memes_image.length);
+  const [meme, setMeme] = useState<string>(memes_image[random]);
 
   return (
-    <>
-      <button className="button" onClick={handleClick}>
+    <div className="flex justify-center">
+      <button
+        className="font-sans font-medium text-xl mt-14 cursor-pointer px-3 bg-gradient-to-r from-cyan-500 to-blue-500 rounded uppercase outline-none w-52"
+        onClick={() => setMeme(memes_image[random])}
+      >
         Click me!
       </button>
       <Image
@@ -21,8 +20,8 @@ export default function Button() {
         alt="meme"
         width={450}
         height={450}
-        className="absolute bottom-3 left-1/2 -translate-x-1/2 max-h-[450px] max-w-[600px]"
+        className="absolute bottom-3 left-1/2 -translate-x-1/2 max-h-[450px] max-w-[600px] w-auto h-auto"
       />
-    </>
+    </div>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,18 +2,17 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="header">
+    <header className="flex w-full items-center justify-evenly h-16 bg-gradient-to-r from-cyan-500 to-blue-500 text-white">
       <a href="https://github.com/2giosangmitom/meme_generator" target="_blank">
         <Image
-          className=""
           src="/troll.webp"
           width={50}
           height={50}
           alt="troll face"
         />
       </a>
-      <h2 className="header--title">Meme generator</h2>
-      <h2 className="header--project">React Project</h2>
+      <h2 className="text-2xl font-medium">Meme generator</h2>
+      <h2 className="text-2xl font-medium">React Project</h2>
     </header>
   );
 }
