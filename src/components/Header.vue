3f219be563bb50823fc6aea6256f5dcca1810d06
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-10-31 22:42:08 +0700

    migrate to Vue

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <button class="count" type="button" @click="count++">
+    count is {{ count }}
+  </button>
+</template>
+
+<style>
+.count {
+  text-transform: uppercase;
+  font-size: 1rem;
+}
+</style>
