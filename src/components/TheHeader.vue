b7095b4f9e6d153b9137036eb3422920e9af7576
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-11-03 23:33:56 +0700

    chore: format

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,7 @@
 <template>
-  <header class="flex w-full items-center justify-evenly h-16 bg-gradient-to-r from-cyan-500 to-blue-500 text-white">
+  <header
+    class="flex w-full items-center justify-evenly h-16 bg-gradient-to-r from-cyan-500 to-blue-500 text-white"
+  >
     <a href="https://github.com/2giosangmitom/meme_generator" target="_blank">
       <img src="/troll.webp" alt="troll face" width="50" height="50" />
     </a>
