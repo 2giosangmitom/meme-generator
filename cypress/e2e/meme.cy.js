f07ebf3e91a0ef34ca775d00928f0aa34a244788
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-11-03 23:29:28 +0700

    test: add 1 case

--- cypress/e2e/meme.cy.js
+++ cypress/e2e/meme.cy.js
@@ -1,8 +1,7 @@
-// https://on.cypress.io/api
-
-describe('My First Test', () => {
+describe('Button', () => {
   it('visits the app root url', () => {
     cy.visit('/')
-    cy.get('button').should("be.visible")
+    cy.get('button').should('be.visible')
+    cy.get('main > div > img').should('be.visible')
   })
 })
