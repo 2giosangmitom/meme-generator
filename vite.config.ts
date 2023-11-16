e7d0c2fff16a70495840bd22da0ff0bcee4f061f
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-11-16 20:31:14 +0700

    add docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  server: {
+    host: true,
+    port: 3000
+  },
+  preview: {
+    host: true,
+    port: 8080
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
