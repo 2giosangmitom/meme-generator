4711dcc557d7801e302ffa2c5d55fd4fab6b83d7
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-12-04 18:52:33 +0700

    Breaking changes!: migrate to [Nuxt.js](https://nuxt.com)

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup>
+import './assets/css/main.css'
+useSeoMeta({
+  title: 'OpenAI'
+})
+</script>
+
+<template>
+  <h1>Hello ae</h1>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  typescript: {
+    strict: true
+  }
+})
