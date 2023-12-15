3a627a5a10c144d606f183ef549bd881fbf87810
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-12-16 00:18:44 +0700

    update project config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,20 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   typescript: {
     strict: true
+  },
+  modules: ['@nuxtjs/google-fonts', '@pinia/nuxt'],
+  googleFonts: {
+    families: {
+      Lobster: true
+    }
+  },
+  runtimeConfig: {
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+    measurementId: process.env.MEASUREMENTID
   }
 })
