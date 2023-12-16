0443c07110feabc3b4ed6b44db6204b3fe31eb6f
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-12-16 13:22:29 +0700

    update app.vue

--- app.vue
+++ app.vue
@@ -1,10 +1,67 @@
-<script setup>
-import './assets/css/main.css'
+<script setup lang="ts">
+import '~/assets/css/main.css'
 useSeoMeta({
   title: 'Meme generator'
 })
 </script>
 
 <template>
-  <h1>Hello ae</h1>
+  <main>
+    <TheHeader />
+    <!-- <span>Total: {{ state.total }}</span> -->
+    <!---->
+    <!-- <div class="gen__meme"> -->
+    <!--   <button class="gen--button" @click="state.newMeme">Generate new meme</button> -->
+    <!--   <img :src="state.meme" /> -->
+    <!-- </div> -->
+  </main>
 </template>
+
+<style scoped>
+.gen__meme {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2rem;
+}
+
+.gen__meme .gen--button {
+  font-weight: 500;
+  font-size: 1.125rem;
+  line-height: 1.75rem;
+  cursor: pointer;
+  padding: 0.25rem 0.5rem;
+  background-color: var(--bg-pornhub);
+  border: solid black 2px;
+  border-radius: 5px;
+}
+
+.gen__meme .gen--button:active {
+  scale: 0.9;
+  transition-duration: 300ms;
+}
+
+.gen__meme img {
+  margin: 2rem 0;
+  max-width: 1000px;
+  height: auto;
+}
+
+@media screen and (max-width: 600px) {
+  .gen__meme img {
+    max-width: 400px;
+  }
+}
+
+@media screen and (max-width: 500px) {
+  .gen__meme img {
+    max-width: 400px;
+  }
+}
+
+@media screen and (max-width: 400px) {
+  .gen__meme img {
+    max-width: 300px;
+  }
+}
+</style>
