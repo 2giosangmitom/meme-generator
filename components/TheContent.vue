59aa78d218b76752562c5a2c53a15e289221000b
Võ Quang Chiến <87252943+2giosangmitom@users.noreply.github.com>
2024-01-20 21:55:47 +0700

    refactor: add `TheContent` component (#56)

---
+++ components/TheContent.vue
@@ -0,0 +1,65 @@
+<script setup>
+defineProps({
+  total: Number,
+  meme: String,
+  getNewMeme: Function,
+});
+</script>
+
+<template>
+  <span>Total: {{ total }}</span>
+
+  <div class="gen__meme">
+    <button class="gen--button" @click="getNewMeme">Generate new meme</button>
+    <img v-bind:src="meme" />
+  </div>
+</template>
+
+<style scoped>
+.gen__meme {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2rem;
+}
+
+.gen__meme .gen--button {
+  font-weight: 500;
+  font-size: 1.125rem;
+  line-height: 1.75rem;
+  cursor: pointer;
+  padding: 0.25rem 0.5rem;
+  background-color: var(--bg-pornhub);
+  border: solid black 2px;
+  border-radius: 5px;
+}
+
+.gen__meme .gen--button:active {
+  scale: 0.9;
+  transition-duration: 300ms;
+}
+
+.gen__meme img {
+  margin: 2rem 0;
+  max-width: 1000px;
+  height: auto;
+}
+
+@media screen and (max-width: 600px) {
+  .gen__meme img {
+    max-width: 400px;
+  }
+}
+
+@media screen and (max-width: 500px) {
+  .gen__meme img {
+    max-width: 400px;
+  }
+}
+
+@media screen and (max-width: 400px) {
+  .gen__meme img {
+    max-width: 300px;
+  }
+}
+</style>
