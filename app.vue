9be22773bcf9b5d50684d8f5948540dd6489a668
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-20 13:55:28 +0700

    feat!: migrate to nuxtjs

--- app.vue
+++ app.vue
@@ -1,13 +1,83 @@
-<script setup lang="ts">
+<script setup>
 const appConfig = useAppConfig();
 useSeoMeta({
   title: appConfig.title,
   description: appConfig.description,
 });
+const state = useMemeStore();
 </script>
 
 <template>
-  <div>
-    <h1>Hello ae phe loi</h1>
-  </div>
+  <main>
+    <TheHeader />
+    <span>Total: {{ state.total }}</span>
+
+    <div class="gen__meme">
+      <button class="gen--button" @click="state.newMeme">
+        Generate new meme
+      </button>
+      <img v-bind:src="state.meme" />
+    </div>
+  </main>
 </template>
+
+<style>
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+:root {
+  --bg-pornhub: #ffa500;
+}
+
+.gen__meme {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2rem;
+}
+
+.gen__meme .gen--button {
+  font-weight: 500;
+  font-size: 1.125rem;
+  line-height: 1.75rem;
+  cursor: pointer;
+  padding: 0.25rem 0.5rem;
+  background-color: var(--bg-pornhub);
+  border: solid black 2px;
+  border-radius: 5px;
+}
+
+.gen__meme .gen--button:active {
+  scale: 0.9;
+  transition-duration: 300ms;
+}
+
+.gen__meme img {
+  margin: 2rem 0;
+  max-width: 1000px;
+  height: auto;
+}
+
+@media screen and (max-width: 600px) {
+  .gen__meme img {
+    max-width: 400px;
+  }
+}
+
+@media screen and (max-width: 500px) {
+  .gen__meme img {
+    max-width: 400px;
+  }
+}
+
+@media screen and (max-width: 400px) {
+  .gen__meme img {
+    max-width: 300px;
+  }
+}
+</style>
